0d4d39f31c2d1b1438fcb28ddc99c2cb75973443
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-07-21 13:38:41 -0400

    Time: 185 ms (5.99%), Space: 52 MB (24.96%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    
+    let maxProfit = 0;
+    let minBuyPrice = Infinity;
+    
+    for (let i = 0; i < prices.length; i++) {
+        if (minBuyPrice > prices[i]) {
+            minBuyPrice = prices[i];
+        }
+        
+        let profit = prices[i] - minBuyPrice;
+        
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+    return maxProfit;
+};
