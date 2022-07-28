239fb442cc791d45c3187f14d8a941e2efc1c89d
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-07-28 16:26:47 -0400

    Time: 160 ms (46.04%), Space: 44.9 MB (90.85%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let start = 0; 
+    let maxLen = 0;
+    let map = new Map();
+    
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (map.get(char) >= start) {
+            start = map.get(char) + 1;
+        }
+        map.set(char, i);
+        maxLen = Math.max(maxLen, i - start + 1)
+    }
+    return maxLen;
+};
