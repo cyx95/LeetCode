d7a94a4e6ef804e1dbc650289bf07a2b33d66f84
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-07-28 19:32:17 -0400

    Time: 84 ms (88.69%), Space: 44.3 MB (74.34%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -3,17 +3,21 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
+    //initialize full sum and left sum
     let leftSum = 0;
     let sum = 0;
     
     for (let i = 0; i < nums.length; i++) {
-        sum += nums[i]
+        //calculate full sum
+        sum += nums[i]                                 
     }
     
     for (let i = 0; i < nums.length; i++) {
-        if (sum - nums[i] - leftSum === leftSum) {
+        //if the sum - current index - left sum === left sum, found the pivot index
+        if (sum - nums[i] - leftSum === leftSum) {    
             return i
         }
+        //if not, add to the left sum
         leftSum += nums[i];
     }
     return -1;
