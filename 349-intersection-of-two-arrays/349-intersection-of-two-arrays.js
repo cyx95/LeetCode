2b6143824af10998399eb001e9e5bb9a101ef3a4
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-07-29 21:59:41 -0400

    Time: 68 ms (92.08%), Space: 43.9 MB (39.76%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -3,6 +3,13 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+// var intersection = function(nums1, nums2) {
+//     return _.intersection(nums1, nums2)
+// };
+
 var intersection = function(nums1, nums2) {
-    return _.intersection(nums1, nums2)
-};
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+    
+    return [...set1].filter(n => set2.has(n))
+}
