855ea02d0773bf6066272daa0ee2a0b5ae7525e1
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-07-31 19:30:49 -0400

    Time: 135 ms (54.66%), Space: 51.2 MB (35.60%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,17 +2,30 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var containsDuplicate = function(nums) {
+//     if (nums.length === 0 || nums.length === 1) {
+//         return false;
+//     }
+    
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[j] === nums[i]) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// };
+
 var containsDuplicate = function(nums) {
-    if (nums.length === 0 || nums.length === 1) {
-        return false;
-    }
+    let hash = {};
     
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[j] === nums[i]) {
-                return true;
-            }
+        if (hash[nums[i]]) {
+            return true;
+        } else {
+            hash[nums[i]] = true;
         }
     }
     return false;
-};
+}
