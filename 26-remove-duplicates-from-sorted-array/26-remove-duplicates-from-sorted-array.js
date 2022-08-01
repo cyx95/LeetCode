4b30856ce3688a52c56f8c125ab2e1bc146df648
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-01 17:45:54 -0400

    Time: 72 ms (96.98%), Space: 44.6 MB (69.43%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    
+    let start = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[i + 1]) {
+            nums[start] = nums[i];
+            start++;
+        }
+    }
+    return start;
+};
