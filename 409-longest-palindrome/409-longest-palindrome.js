14e7dfff00d4911da71ab28e5efe28d5906bacb1
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-01 17:22:00 -0400

    Time: 69 ms (95.00%), Space: 43.8 MB (61.77%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    
+    let set = new Set();
+    let count = 0;
+    
+    for (let char of s) {
+        if (set.has(char)) {
+            count += 2;
+            set.delete(char)
+        } else {
+            set.add(char)
+        }
+    }
+    return count + (set.size > 0 ? 1 : 0);
+};
