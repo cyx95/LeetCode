8a218118241bc3aa12eb731c52b6370e71495689
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-07-31 22:50:26 -0400

    Time: 109 ms (63.57%), Space: 43.2 MB (30.98%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -22,12 +22,12 @@ var twoSum = function(nums, target) {
 
 var twoSum = function(nums, target) {
     
-    let map = new Map();
+    let map = new Map()
     
     for (let i = 0; i < nums.length; i++) {
         let need = target - nums[i];
-        if (map.has(need) && map.get(need) !== i) {
-            return [i, map.get(need)];
+        if (map.has(need) && map.get(need) != i) {
+            return [map.get(need), i]
         }
         map.set(nums[i], i)
     }
