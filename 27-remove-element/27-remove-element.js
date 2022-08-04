89ab1818862b06b08e849835698b88a99791714f
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-03 21:30:55 -0400

    Time: 90 ms (51.87%), Space: 42 MB (59.54%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -3,13 +3,24 @@
  * @param {number} val
  * @return {number}
  */
+// var removeElement = function(nums, val) {
+//     let i = 0;
+//     for (let j = 0; j < nums.length; j++) {
+//         if (nums[j] !== val) {
+//             nums[i] = nums[j];
+//             i++
+//         }
+//     }
+//     return i;
+// };
+
 var removeElement = function(nums, val) {
-    let i = 0;
-    for (let j = 0; j < nums.length; j++) {
-        if (nums[j] !== val) {
-            nums[i] = nums[j];
-            i++
+   
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i, 1)
+            i--
         }
     }
-    return i;
-};
+    return nums.length;
+}
