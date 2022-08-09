fb3527fcd26d7a1aa6be22d2b5d787727cfa84ec
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-09 14:31:52 -0400

    Time: 70 ms (94.99%), Space: 44.6 MB (16.57%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -12,8 +12,8 @@
  */
 var mergeTwoLists = function(list1, list2) {
    
-    if (list1 === null) return list2;
-    if (list2 === null) return list1;
+    if (list1 === null) return list2
+    if (list2 === null) return list1
     
     if (list1.val < list2.val) {
         list1.next = mergeTwoLists(list1.next, list2);
