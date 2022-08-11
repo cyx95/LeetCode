e052166a668a819ecfc899ca2338971cdaf04e8e
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-10 22:00:17 -0400

    Time: 56 ms (98.65%), Space: 42 MB (69.96%) - LeetHub

---
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    let result = [];
+    dfs(root);
+    
+    function dfs(root) {
+        if (root !== null) {
+            dfs(root.left);
+            dfs(root.right);
+            result.push(root.val)
+        }
+    }
+    return result;
+};
