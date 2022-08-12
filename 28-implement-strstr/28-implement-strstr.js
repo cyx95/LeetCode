5d3e3fdc0de398e5775735542f3ea1f3330b0350
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-11 21:57:39 -0400

    Time: 94 ms (42.06%), Space: 42 MB (51.75%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -3,14 +3,22 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
+// var strStr = function(haystack, needle) {
     
-    for (let i = 0 ; i < haystack.length; i++) {
-        for (let j = 0; j < needle.length; j++) {
-            if (haystack[i + j] !== needle[j]) break
+//     for (let i = 0 ; i < haystack.length; i++) {
+//         for (let j = 0; j < needle.length; j++) {
+//             if (haystack[i + j] !== needle[j]) break
             
-            if (j + 1 === needle.length) return i
-        }  
+//             if (j + 1 === needle.length) return i
+//         }  
+//     }
+//     return -1
+// };
+
+var strStr = function (haystack, needle) {
+    if (needle === '') {
+        return 0
+    } else {
+        return haystack.indexOf(needle)
     }
-    return -1
-};
+}
