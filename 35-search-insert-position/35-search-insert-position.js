f6e4f64db0b318efd64c694fcee7c881d5d50217
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-11 21:53:09 -0400

    Time: 95 ms (37.71%), Space: 41.8 MB (93.92%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -5,10 +5,18 @@
  */
 var searchInsert = function(nums, target) {
     
-    for (let i = 0; i < nums.length; i++) {
-        if (target <= nums[i]) {
-            return i
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((right + left) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         } 
     }
-    return nums.length;
+    return right + 1;
 };
