f7ac16840e3e64248ce678f621b07d75b6536d30
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-17 11:28:19 -0400

    Time: 101 ms (42.59%), Space: 45.2 MB (45.63%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,21 +4,17 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
     let left = 0;
     let right = nums.length - 1;
-    
-    while (left <= right) {
-        let mid = Math.floor( (right + left) / 2)
-        
+    for (let i = 0; i < nums.length; i++) {
+        let mid = Math.floor((right + left) / 2);
         if (nums[mid] === target) {
-            return mid
-        }    
-        if (nums[mid] < target) {
+            return mid;
+        } else if (nums[mid] < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
-        }    
+        }
     }
     return -1;
 };
