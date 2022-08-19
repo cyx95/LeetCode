533e6a0a78d0b8d1e15806d1a54eab383880f0ea
Chen Ying Xin <96203831+cyx95@users.noreply.github.com>
2022-08-19 12:39:44 -0400

    Time: 90 ms (60.52%), Space: 42.4 MB (65.17%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,22 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
     if (s.length === 0) {
         return false;
     }
     
-    const parenthesis = {
-        "{": "}",
-        "[": "]",
-        "(": ")"
+    let parenthesis = {
+        "{" : "}",
+        "[" : "]",
+        "(" : ")"
     }
     
-    const stack = [];
+    let stack = [];
     
     for (let i = 0; i < s.length; i++) {
         if (s[i] in parenthesis) {
-            stack.push(s[i])
+            stack.push(s[i]);
         } else if (s[i] !== parenthesis[stack.pop()]) {
             return false;
         }
